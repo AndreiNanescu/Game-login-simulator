b9b72dce3da2b6c3fa442ad392aa4584c778514e
AndreiNanescu <91884910+AndreiNanescu@users.noreply.github.com>
2022-05-27 11:48:15 +0300

    Created a random username generator.

--- shard simulator/player.cpp
+++ shard simulator/player.cpp
@@ -1,10 +1,20 @@
 #include "player.h"
 #include <ctime>
+const int MAX{ 26 };
 player player::generatePlayer()
 {
-	player auxClass;
-	int randomNumber;
 	srand(time(0));
+	player auxClass;
+	//Username generator
+	char alphabet[MAX] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+						  'h', 'i', 'j', 'k', 'l', 'm', 'n',
+						  'o', 'p', 'q', 'r', 's', 't', 'u',
+						  'v', 'w', 'x', 'y', 'z' };
+	int randomNameLength{ rand() % 5 + 3 };
+	for (int i = 0; i < randomNameLength; i++)
+		auxClass.userName += alphabet[rand() % MAX];
+	//IP generator
+	int randomNumber;
 	for (int i = 0; i < 4; i++)
 	{
 		randomNumber = rand() % 255;
@@ -12,4 +22,6 @@ player player::generatePlayer()
 		auxClass.IP += '.';
 	}
 	auxClass.IP.pop_back();
+
+	return auxClass;
 }
